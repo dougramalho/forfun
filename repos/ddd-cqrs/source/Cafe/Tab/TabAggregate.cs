f379250ea10993790813ec496f9d88147ce0300c
Douglas Jose Ramalho Araujo <douglas.ramalho@squadra.com.br>
2018-03-01 11:04:13 -0300

    Initial base class, and first test

---
+++ repos/ddd-cqrs/source/Cafe/Tab/TabAggregate.cs
@@ -0,0 +1,25 @@
+﻿using Edument.CQRS;
+using Events.Tab;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe.Tab
+{
+    public class TabAggregate : Aggregate, IHandleCommand<OpenTab>
+    {
+
+        public IEnumerable Handle(OpenTab c)
+        {
+            yield return new TabOpened
+            {
+                Id = c.Id,
+                TableNumber = c.TableNumber,
+                Waiter = c.Waiter
+            };
+        }
+    }
+}
---
+++ repos/ddd-cqrs/source/CafeTests/TabTests.cs
@@ -0,0 +1,47 @@
+﻿using Cafe.Tab;
+using Edument.CQRS;
+using Events.Tab;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeTests
+{
+    [TestFixture]
+    public class TabTests : BDDTest<TabAggregate>
+    {
+        private Guid testId;
+        private int testTable;
+        private string testWaiter;
+
+        [SetUp]
+        public void Setup()
+        {
+            testId = Guid.NewGuid();
+            testTable = 42;
+            testWaiter = "Derek";
+        }
+
+        [Test]
+        public void CanOpenANewTab()
+        {
+            Test(
+                Given(),
+                When(new OpenTab
+                {
+                    Id = testId,
+                    TableNumber = testTable,
+                    Waiter = testWaiter
+                }),
+                Then(new TabOpened
+                {
+                    Id = testId,
+                    TableNumber = testTable,
+                    Waiter = testWaiter
+                }));
+        }
+    }
+}
