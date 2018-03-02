a2414c8af6624bb4191ec1abc7eba15fd8cd42d8
Douglas Jose Ramalho Araujo <douglas.ramalho@squadra.com.br>
2018-03-02 14:31:01 -0300

    Creating tests and handle Table

--- repos/ddd-cqrs/source/Cafe/Tab/TabAggregate.cs
+++ repos/ddd-cqrs/source/Cafe/Tab/TabAggregate.cs
@@ -9,8 +9,12 @@ using System.Threading.Tasks;
 
 namespace Cafe.Tab
 {
-    public class TabAggregate : Aggregate, IHandleCommand<OpenTab>
+    public class TabAggregate : Aggregate, 
+        IHandleCommand<OpenTab>,
+        IHandleCommand<PlaceOrder>,
+        IApplyEvent<TabOpened>
     {
+        private bool open = false;
 
         public IEnumerable Handle(OpenTab c)
         {
@@ -20,6 +24,34 @@ namespace Cafe.Tab
                 TableNumber = c.TableNumber,
                 Waiter = c.Waiter
             };
+        }
+
+        public IEnumerable Handle(PlaceOrder c)
+        {
+            if (!open)
+                throw new TabNotOpen();
+
+            var drink = c.Items.Where(x => x.IsDrink).ToList();
+
+            if (drink.Any())
+                yield return new DrinksOrdered
+                {
+                    Id = c.Id,
+                    Items = drink
+                };
+
+            var food = c.Items.Where(i => !i.IsDrink).ToList();
+            if (food.Any())
+                yield return new FoodOrdered
+                {
+                    Id = c.Id,
+                    Items = food
+                };
+        }
+
+        public void Apply(TabOpened e)
+        {
+            open = true;
         }
     }
 }
---
+++ repos/ddd-cqrs/source/Events/Tab/Shared.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events.Tab
+{
+    public class OrderedItem
+    {
+        public int MenuNumber;
+        public string Description;
+        public bool IsDrink;
+        public decimal Price;
+    }
+
+    public class DrinksOrdered
+    {
+        public Guid Id;
+        public List<OrderedItem> Items;
+    }
+
+    public class FoodOrdered
+    {
+        public Guid Id;
+        public List<OrderedItem> Items;
+    }
+}
